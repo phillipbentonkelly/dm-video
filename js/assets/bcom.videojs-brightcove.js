638d0675c8f388fc0ddf5c980cb48a42eedc1576
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2015-02-26 15:28:00 -0500

    Updated GH-Pages with the latest version of this repo that can be used for QA.

--- js/assets/bcom.videojs-brightcove.js
+++ js/assets/bcom.videojs-brightcove.js
@@ -4,12 +4,29 @@ var videoJSWidgetObj = {};
 (function( win, $, undefined ) {
     'use strict';
 
+    // Setup Custom Events for 
+    var evt_fetchDataById = document.createEvent('Event');
+    	evt_fetchDataById.initEvent('fetchedDataById', true, true);
+    
+    var evt_initPlayers = document.createEvent('Event');
+    	evt_initPlayers.initEvent('playersInitted', true, true);
+    
+    var evt_initSinglePlayer = document.createEvent('Event');
+    	evt_initSinglePlayer.initEvent('playerInitted', true, true);
+    
     var videojsWidget = {};
     var _data = {
     	apis: {
-    		media: {}, 
-    		tokens: ['mSs7049lqF2NZe_nAJNIDg1FQDco9YtZEk8YfbwYAEo.', 'LQQ1lepeYOtRowHPofjWKf4Cmw54Sb3ZJo1pXa9so24.']
-    	}
+    		media: {
+    			commands: [ 'search_videos', 'find_video_by_id', 'find_all_videos' ],
+    			filters: [ 'video_id', 'all=tag:', 'none=tag:', 'sort_by=', 'page_number=', 'page_size=', 'get_item_count=', '=CREATION_DATE:' ]
+    		}, 
+    		tokens: [ 'mSs7049lqF2NZe_nAJNIDg1FQDco9YtZEk8YfbwYAEo.', 'LQQ1lepeYOtRowHPofjWKf4Cmw54Sb3ZJo1pXa9so24.', 'UtStY5Lr9ydJtzMcHFeumhdUrAmZC37febmmPfpMUBY=' ],
+    		baseURL: '//api.brightcove.com/services/library?command=',
+    		protocol: 'http:'
+    	},
+    	unInittedPlayers: [],
+    	playersObjs: {}
     };
 
     VideoJS = function(){
@@ -19,43 +36,122 @@ var videoJSWidgetObj = {};
 
     VideoJS.prototype = {
     	init: function( params ){
-    		/*
-				params = {
-					featuredVid: { videojs('featutedVid') },
-					featuredIndex: 0,
-					vidsObjs: [ { videojs('featuredVid') }, { videojs('sidebarPlayer') } ]
-				}
-    		*/
-    		//(!params.videoObjs)? {} : for(var u = 0; u < params.videoObjs.length; u++){ console.log(params.videoObjs[u]); }
-    		BCMAPI.token = _data.apis.tokens[0];
-    		BCMAPI.callback = "this.handle";
-    		BCMAPI.find("video_by_id", 4030141014001);
+    		this.eventHandlers();
 
-    		console.log(_data.apis.tokens[0]);
+    		if(typeof(params.videoObjsRefs) !== 'undefined'){
+				BCMAPI.token = _data.apis.tokens[2];
+	    		_data.unInittedPlayers = params.videoObjsRefs;
+	    		this.processVidObjs();
+    		}else{}
+    	}, 
+    	processVidObjs: function(){
+    		if(_data.unInittedPlayers.length > 0){
+    			this.initVideo(_data.unInittedPlayers[0]);
+    		}else{
+    			document.dispatchEvent(evt_initPlayers);
+    		}
     	},
-    	handle: function(pResponse) {
-	        for (var pVideo in pResponse.items) {
-	            alert(pVideo.name);
-	        }
+    	eventHandlers: function(){
+    		var thisRef = this;
+
+    		document.addEventListener('fetchedDataById', function (e) {
+    			/*console.log('fetchedDataById');*/
+    		}, false);
+    		document.addEventListener('playerInitted', function (e) {
+    			/*console.log('playerInitted: Player at the current Index has been initted and recorded.');*/
+    			_data.unInittedPlayers.shift();
+    			thisRef.processVidObjs();
+    		}, false);
+    		document.addEventListener('playersInitted', function (e) {
+    			/*console.log('playersInitted: All Players are Ready.');
+    			console.log(_data.playersObjs);
+    			console.log(_data.playersObjs[4030141014001].bcId);*/
+    			alert(_data.playersObjs[4030141014001].bcId);
+    		}, false);
+    	},
+    	fetchedDataByIdSuccess: function(pResponse){
+    		var nodeName1 = 'data';
+	    	_data.playersObjs[pResponse.id][nodeName1] = pResponse;
+
+	    	document.dispatchEvent(evt_fetchDataById);
+	    	document.dispatchEvent(evt_initSinglePlayer);
 	    },
-    	initVideo: function( params ){
-    		console.log("initVideo....");
+    	initVideo: function( id ){
+    		var jqObj = $('#' + id);
+    		var video_id = jqObj.attr('data-video-id');
+    		var nodeName1 = 'playerObjId';
+    		var nodeName2 = 'videojsObj';
+    		var nodeName3 = 'bcId';
+    		var tempVidObj = videojs(id);
+    		
+    		_data.playersObjs[video_id] = {};
+    			_data.playersObjs[video_id][nodeName1] = id;
+    			_data.playersObjs[video_id][nodeName2] = tempVidObj;
+    			_data.playersObjs[video_id][nodeName3] = video_id;
+
+    		BCMAPI.callback = "videoJSWidgetObj.fetchedDataByIdSuccess";
+    		BCMAPI.find("video_by_id", video_id);
+    	},
+    	getVideos: function( params ){
+    		var thisRef = this;
+    		var retVar = {};
+    		var thisApiUrl = _data.apis.protocol + _data.apis.baseURL + _data.apis.media.commands[0] + '&token=' + _data.apis.tokens[2];
+    			if(typeof(params.page_size) !== 'undefined')
+    				thisApiUrl = thisApiUrl + '&' + _data.apis.media.filters[5] + params.page_size;
+    			if(typeof(params.sort) !== 'undefined')
+    				thisApiUrl = thisApiUrl + '&sort_by' + _data.apis.media.filters[7] + params.sort;
+    			//if(typeof(params.retVar) !== 'undefined')
+
+    			//console.log(thisApiUrl);
+
+    		$.ajax({
+    			url: thisApiUrl,
+    			dataType: 'json'
+    		}).done(function(data){
+    			/*console.log("getVideos sucess");
+    			console.log(data);
+    			bc_contentLists.recent = data;*/
+    			if(typeof(params.retVar) !== 'undefined')
+    				params.retVar = data;
+    		}).fail(function(){
+    			//console.log("getVideos failed");
+    		});
+
+    		return retVar;
     	}
     };
 })(window, jQuery);
 
+var bc_contentLists = {
+	'recent': {}
+};
+
 $(function(){
 	videoJSWidgetObj = VideoJS();
-	videoJSWidgetObj.init();
+	videoJSWidgetObj.init(
+		{
+			featuredIndex: 0,
+			videoObjsRefs: [ 'featuredPlayer', 'sidebarPlayer'/**/ ]
+		}
+	);
+	// Get Videos to populate the carousels: returns JSON
+	/*bc_contentLists.recent = videoJSWidgetObj.getVideos({
+		'retVar': bc_contentLists.recent
+		'page_size': 20,
+		'sort': 'DESC'
+	});*/
+	videoJSWidgetObj.getVideos({ 'retVar': bc_contentLists.recent, 'page_size': 20, 'sort': 'DESC' });
+		/*console.log("bc_contentLists.recent: ------");
+		console.log(bc_contentLists.recent);*/
 });
 
-function handle (pResponse) {
-    /*for (var pVideo in pResponse.items) {
-        alert(pVideo.name);
-    }*/
 
-    console.log(pResponse);
-}
+
+/*var thisApi = _data.apis;
+console.log('Search Videos: ' + thisApi.protocol + thisApi.baseURL + thisApi.media.commands[0] + '&token=' + thisApi.tokens[2] );
+console.log('Search Videos: ' + thisApi.protocol + thisApi.baseURL + thisApi.media.commands[1] + '&' + thisApi.media.filters[0] + '=' + '4080467172001' + '&token=' + thisApi.tokens[2]);
+videoJSWidgetObj.getVideos({'page_size': 20,'sort': 'DESC'})*/
+
 
 
 
